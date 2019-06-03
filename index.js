be1db76c06a3e5f5f1d50b48dd02160a6b84b72e
Aaron Arinder <aaronarinder@gmail.com>
2019-06-03 12:55:52 -0400

    twittah: initial commit   - commander in place     - cli interface   - Twitter package in place     - handles auth/access/requests

---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+require('./env.js');
+
+const program = require('commander');
+const Twitter = require('twitter');
+const config = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret,
+};
+const twitter = new Twitter(config);
+
+
+/*
+ * COMMANDER
+ * */
+
+program.version('0.0.1');
+
+program
+  .option('-s, --search <query | count>', 'query and [optional] count', searchStatuses);
+
+program.parse(process.argv);
+
+
+
+/*
+ * HELPER FNS
+ * */
+
+function searchStatuses (valueString) {
+  const [ query, count ] = valueString.split(' | ');
+  const params = {
+    q: query,
+    count: count || 10,
+    result_type: 'recent',
+    lang: 'en',
+  };
+
+  twitter.get('search/tweets', params, function (err, data, response) {
+    if (!err) {
+      data.statuses.forEach(d => {
+        console.log(`${d.user.screen_name} :: ${d.text}\n\n\n`);
+      });
+    } else {
+      console.log('err', err);
+    }
+  });
+}
+
